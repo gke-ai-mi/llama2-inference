9bef8d4653568b05778caaca64e7457a51814a03
Rick(Rugui) Chen <rickruguichen@google.com>
2023-10-24 13:39:59 -0400

    vLLM deployment

--- create-cluster.sh
+++ create-cluster.sh
@@ -1,1 +1,33 @@
-gcloud container clusters create pytorch-training-cluster     --num-nodes=2     --zone=us-west1-b     --accelerator="type=nvidia-tesla-t4,count=2,gpu-driver-version=default"  --machine-type="n1-standard-8"     --scopes="gke-default,storage-rw"
+# for tesla-t4
+#gcloud container clusters create pytorch-training-cluster     --num-nodes=2     --zone=us-west1-b     --accelerator="type=nvidia-tesla-t4,count=2,gpu-driver-version=default"  --machine-type="n1-standard-8"     --scopes="gke-default,storage-rw"
+
+
+# for L4
+export REGION=us-central1
+export PROJECT_ID=$(gcloud config get project)
+
+gcloud container clusters create l4-demo --location ${REGION} \
+  --workload-pool ${PROJECT_ID}.svc.id.goog \
+  --enable-image-streaming --enable-shielded-nodes \
+  --shielded-secure-boot --shielded-integrity-monitoring \
+  --enable-ip-alias \
+  --node-locations=$REGION-a \
+  --workload-pool=${PROJECT_ID}.svc.id.goog \
+  --addons GcsFuseCsiDriver   \
+  --no-enable-master-authorized-networks \
+  --machine-type n2d-standard-4 \
+  --num-nodes 1 --min-nodes 1 --max-nodes 5 \
+  --ephemeral-storage-local-ssd=count=2 \
+  --enable-ip-alias \
+  --enable-private-nodes  \
+  --master-ipv4-cidr 172.16.0.32/28
+
+gcloud container node-pools create g2-standard-24 --cluster l4-demo \
+  --accelerator type=nvidia-l4,count=2,gpu-driver-version=latest \
+  --machine-type g2-standard-24 \
+  --ephemeral-storage-local-ssd=count=2 \
+  --enable-autoscaling --enable-image-streaming \
+  --num-nodes=0 --min-nodes=0 --max-nodes=3 \
+  --shielded-secure-boot \
+  --shielded-integrity-monitoring \
+  --node-locations $REGION-a,$REGION-b --region $REGION --spot
