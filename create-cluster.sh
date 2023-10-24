4622688cac9cb2861f76fbbe443327e4548d251a
Rick(Rugui) Chen <rickruguichen@google.com>
2023-10-24 15:18:31 -0400

    vLLM deployment

--- create-cluster.sh
+++ create-cluster.sh
@@ -1,5 +1,5 @@
 # for tesla-t4
-#gcloud container clusters create pytorch-training-cluster     --num-nodes=2     --zone=us-west1-b     --accelerator="type=nvidia-tesla-t4,count=2,gpu-driver-version=default"  --machine-type="n1-standard-8"     --scopes="gke-default,storage-rw"
+#gcloud container clusters create llama2-inference-cluster --num-nodes=2     --zone=us-west1-b     --accelerator="type=nvidia-tesla-t4,count=2,gpu-driver-version=default"  --machine-type="n1-standard-8"  --enable-private-nodes --master-ipv4-cidr 172.16.0.32/28  --scopes="gke-default,storage-rw"
 
 
 # for L4
