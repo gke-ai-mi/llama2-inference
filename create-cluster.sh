9e68c13545fc42b950b365eb8f678892590fa423
Rick(Rugui) Chen <rickruguichen@google.com>
2023-10-24 09:55:59 -0400

    test deployment

--- create-cluster.sh
+++ create-cluster.sh
@@ -1,1 +1,1 @@
-gcloud container clusters create pytorch-training-cluster     --num-nodes=2     --zone=us-west1-b     --accelerator="type=nvidia-t4,count=2,gpu-driver-version=default"  --machine-type="n1-standard-8"     --scopes="gke-default,storage-rw"
+gcloud container clusters create pytorch-training-cluster     --num-nodes=2     --zone=us-west1-b     --accelerator="type=nvidia-tesla-t4,count=2,gpu-driver-version=default"  --machine-type="n1-standard-8"     --scopes="gke-default,storage-rw"
