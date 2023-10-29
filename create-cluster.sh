3e13adfafacfe38b7da6239cf8dd52e62f512188
Rick(Rugui) Chen <rickruguichen@google.com>
2023-10-29 18:33:15 -0400

    update README

--- create-cluster.sh
+++ create-cluster.sh
@@ -5,7 +5,7 @@
 #gcloud container clusters create llama2-inference-cluster --num-nodes=1 --min-nodes=1 --max-nodes=3  --zone=us-central1-a     --accelerator="type=nvidia-l4,count=2,gpu-driver-version=default"  --machine-type="g2-standard-24" --enable-ip-alias --scopes="gke-default,storage-rw"
 
 # for L4 and spot private cluster
-export REGION=us-centra1
+export REGION=us-central1
 export PROJECT_ID=$(gcloud config get project)
 
 gcloud container clusters create llm-inference-t4 --location ${REGION} \
@@ -18,18 +18,11 @@ gcloud container clusters create llm-inference-t4 --location ${REGION} \
   --addons GcsFuseCsiDriver   \
   --no-enable-master-authorized-networks \
   --machine-type n2d-standard-4 \
-  --num-nodes 1 --min-nodes 1 --max-nodes 5 \
+  --num-nodes 1 --min-nodes 1 --max-nodes 3 \
   --ephemeral-storage-local-ssd=count=2 \
   --enable-ip-alias \
   --enable-private-nodes  \
-  --master-ipv4-cidr 172.16.0.32/28
+  --master-ipv4-cidr 172.16.0.32/28 \
+  --scopes="gke-default,storage-rw"
 
-gcloud container node-pools create llm-inference-t4 --cluster llm-inference-t4 \
-  --accelerator type=nvidia-tesla-t4,count=2,gpu-driver-version=latest \
-  --machine-type n1-standard-8 \
-  --ephemeral-storage-local-ssd=count=2 \
-  --enable-autoscaling --enable-image-streaming \
-  --num-nodes=0 --min-nodes=0 --max-nodes=3 \
-  --shielded-secure-boot \
-  --shielded-integrity-monitoring \
-  --node-locations $REGION-a,$REGION-b --region $REGION --spot
+gcloud container node-pools create g2-standard-24 --cluster llama-inference-t4  --accelerator type=nvidia-l4,count=2,gpu-driver-version=latest   --machine-type g2-standard-24   --ephemeral-storage-local-ssd=count=2   --enable-autoscaling --enable-image-streaming   --num-nodes=0 --min-nodes=0 --max-nodes=3   --shielded-secure-boot   --shielded-integrity-monitoring   --node-locations $REGION-a,$REGION-b --region $REGION --spot
